01a9c3a11049c7fe5057c00088708af8ffa5327f
HiImHibiki <mario.rufisanto@gmail.com>
2021-10-13 15:30:35 +0700

    another export fix

--- src/PlaylistsService.js
+++ src/PlaylistsService.js
@@ -7,13 +7,6 @@ class PlaylistsService {
 
   async getPlaylists(playlistId) {
     const query = {
-      // text: `SELECT playlists.* FROM playlists
-      // LEFT JOIN collaborations ON collaborations.playlist_id = playlists.id
-      // WHERE (playlists.owner = $1 OR collaborations.user_id = $1)
-	    // AND playlists.id = $2
-      // GROUP BY playlists.id`,
-      // values: [userId, playlistId],
-
       text: `SELECT songs.id, songs.title, songs.performer FROM playlistsongs 
       LEFT JOIN songs ON songs.id = playlistsongs.song_id 
       WHERE playlistsongs.playlist_id = $1`,
